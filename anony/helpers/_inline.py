b37d2dc72ea164fe90a89a6353a867d1539f79fb
bisug <lannsnolo@gmail.com>
2026-06-10 18:37:50 +0545

    Refactor inline keyboard logic to simplify channel source handling

--- anony/helpers/_inline.py
+++ anony/helpers/_inline.py
@@ -148,16 +148,7 @@ class Inline:
                 self.ikb(text=lang["channel"], url=config.SUPPORT_CHANNEL),
             ],
         ]
-        if private:
-            rows += [
-                [
-                    self.ikb(
-                        text=lang["source"],
-                        url="https://github.com/AnonymousX1025/AnonXMusic",
-                    )
-                ]
-            ]
-        else:
+        if not private:
             rows += [[self.ikb(text=lang["language"], callback_data="language")]]
         return self.ikm(rows)
 
